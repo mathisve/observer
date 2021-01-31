91677e0b8785a7500f2476f5c5a4541ec5dc9dce
Mathis Van Eetvelde <mathis.vaneetvelde@gmail.com>
2021-01-31 16:08:31 +0100

    added tf backend

--- attachmentLambda/go.mod
+++
@@ -1,8 +0,0 @@
-module contentLambda
-
-go 1.14
-
-require (
-	github.com/aws/aws-lambda-go v1.22.0
-	github.com/aws/aws-sdk-go v1.36.23
-)
--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "gusbackend"
+    key = "terraform"
+    region = "eu-central-1"
+  }
+}
+
 variable "region" {
   default = "eu-central-1"
 }
@@ -9,7 +17,7 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_dynamodb_table" "MessagesTable" {
-  name = "observerBotMessages"
+  name = "gusBotMessages"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "authorId"
   range_key = "messageId"
@@ -29,12 +37,12 @@ resource "aws_dynamodb_table" "MessagesTable" {
   }
 
   tags = {
-    "DDBTableGroupKey-observerBot" = "observerBot"
+    "DDBTableGroupKey-observerBot" = "gusBot"
   }
 }
 
 resource "aws_dynamodb_table" "MemberAdd" {
-  name = "observerMemberAdd"
+  name = "gusMemberAdd"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "userId"
   range_key = "timestamp"
@@ -54,62 +62,11 @@ resource "aws_dynamodb_table" "MemberAdd" {
   }
 
   tags = {
-    "DDBTableGroupKey-observerBot" = "observerBot"
+    "DDBTableGroupKey-observerBot" = "gusBot"
   }
 }
-
-resource "aws_dynamodb_table" "AttachmentTable" {
-  name = "observerBotAttachments"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "authorId"
-  range_key = "messageId"
-
-  attribute {
-    name = "authorId"
-    type = "S"
-  }
-
-  attribute {
-    name = "messageId"
-    type = "S"
-  }
-
-  point_in_time_recovery {
-    enabled = true
-  }
-
-  tags = {
-    "DDBTableGroupKey-observerBot" = "observerBot"
-  }
-}
-
-resource "aws_dynamodb_table" "VoiceEventsTable" {
-  name = "observerBotVoiceEvents"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "userId"
-  range_key = "timestamp"
-
-  attribute {
-    name = "userId"
-    type = "S"
-  }
-
-  attribute {
-    name = "timestamp"
-    type = "N"
-  }
-
-  point_in_time_recovery {
-    enabled = true
-  }
-
-  tags = {
-    "DDBTableGroupKey-observerBot" = "observerBot"
-  }
-}
-
 resource "aws_dynamodb_table" "BannedMembers" {
-  name = "observerBannedMembers"
+  name = "gusBannedMembers"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "userId"
   range_key = "timestamp"
@@ -129,35 +86,7 @@ resource "aws_dynamodb_table" "BannedMembers" {
   }
 
   tags = {
-    "DDBTableGroupKey-observerBot" = "observerBot"
-  }
-}
-
-resource "aws_s3_bucket" "AttachmentBucket" {
-  bucket = "observerbotattachments"
-  acl    = "private"
-}
-
-resource "aws_lambda_function" "AttachmentDownloader" {
-  function_name = "observerBotAttachments"
-
-  filename = "attachmentLambda/attachmentLambda.zip"
-  source_code_hash = filebase64sha256("attachmentLambda/attachmentLambda.zip")
-
-  handler = "contentLambda"
-  role = "arn:aws:iam::043039367084:role/service-role/discordSaveAttachments-role-paknnvaq"
-
-  runtime = "go1.x"
-  timeout = 1
-  memory_size = 512
-
-
-  environment {
-    variables = {
-      REGION = "eu-central-1",
-      BUCKET = aws_s3_bucket.AttachmentBucket.bucket
-      TABLE = aws_dynamodb_table.AttachmentTable.name
-    }
+    "DDBTableGroupKey-observerBot" = "gusBot"
   }
 }
 
